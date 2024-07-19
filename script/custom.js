010bd81d6b7ea9086d0409ef16ca00e33647c483
eeazzy <khs2498@gmail.com>
2024-07-19 16:16:52 +0900

    Initial commit

---
+++ script/custom.js
@@ -0,0 +1,8 @@
+$('p').css({'display':'none'})
+$('.show').click(function(){
+    $('p').css({'display':'block'})
+});
+
+$('.hide').click(function(){
+    $('p').css({'display':'none'})
+});
---
+++ script/jquery_function.js
@@ -0,0 +1,6 @@
+$('.show').click(function(){
+    $('div').show()
+})
+$('.hide').click(function(){
+    $('div').hide()
+})
